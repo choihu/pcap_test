09da1e5298e395b6ddf4d3bb53d16865808c16d9
choihu <guddns789@gmail.com>
2019-09-21 23:28:57 -0700

    Add data capture and modify some detail.

--- main.cpp
+++ main.cpp
@@ -33,13 +33,19 @@ int main(int argc, char* argv[]) {
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
 
+
+    //print mac address.
     unsigned char src_mac[6], dest_mac[6];
+
     memcpy(src_mac, packet+6, 6);
     memcpy(dest_mac, packet, 6);
     printf("Source mac is %x:%x:%x:%x:%x:%x and destination mac is %x:%x:%x:%x:%x:%x\n", src_mac[0], src_mac[1], src_mac[2], src_mac[3], src_mac[4], src_mac[5], dest_mac[0], dest_mac[1], dest_mac[2], dest_mac[3], dest_mac[4], dest_mac[5]);
-        
+
+
+    //print ip address.
     unsigned char is_ip[2];
     unsigned char ip_check[2] = {0x08, 0x00};
+
     memcpy(is_ip, packet+12, 2);
     if(!memcmp(is_ip, ip_check, 2)) {
       unsigned char ip1[4], ip2[4];
@@ -47,24 +53,38 @@ int main(int argc, char* argv[]) {
       memcpy(ip2, packet+30, 4);
 
       printf("Source ip is %d.%d.%d.%d and destination ip is %d.%d.%d.%d\n", ip1[0], ip1[1], ip1[2], ip1[3], ip2[0], ip2[1], ip2[2], ip2[3]);
-    }
+    
 
-    unsigned char is_tcp[1];
-    unsigned char tcp_check[1] = {0x06};
-    memcpy(is_tcp, packet+23, 1);
-    if(!memcmp(is_tcp, tcp_check, 1)) {
-      unsigned char port1[2], port2[2];
-      memcpy(port1, packet+34, 2);
-      uint16_t* src_port = (uint16_t*) port1;
-      memcpy(port2, packet+36, 2);
-      uint16_t* dest_port = (uint16_t*) port2;
-      printf("Source port is %d and destination port is %d\n", ntohs(*src_port), ntohs(*dest_port));
-    }
+      //print tcp port;
+      unsigned char is_tcp[1];
+      unsigned char tcp_check[1] = {0x06};
 
-//    printf("The data from the first 32 bytes is %x\n");
-    printf("===================================================================\n");
+      memcpy(is_tcp, packet+23, 1);
+      if(!memcmp(is_tcp, tcp_check, 1)) {
+        unsigned char port1[2], port2[2];
+        memcpy(port1, packet+34, 2);
+        uint16_t* src_port = (uint16_t*) port1;
+        memcpy(port2, packet+36, 2);
+        uint16_t* dest_port = (uint16_t*) port2;
+        printf("Source port is %d and destination port is %d", ntohs(*src_port), ntohs(*dest_port));    //network order to host order.
+        
+        int ip_header = (int)(packet[14] & 0x0f) * 5;
+        int tcp_header = (int)((packet[14 + ip_header + 12] & 0xf0) >> 4) * 5;
+        int header_length = 14 + ip_header + tcp_header;
+        unsigned char data[32];
+        memcpy(data, packet+header_length, 32);
+        for(int i = 0; i < 32; i++) {
+          if(i == 0)
+            printf("\nThe data from the first 32 bytes is %x", data[i]);
+          else
+            printf("%x", data[i]);
+        }
+        printf("\n");
+      }
+    }
+    printf("=============================================================\n");
   }
-
+  
   pcap_close(handle);
   return 0;
 }
