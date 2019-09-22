9101b9126cab06c1b33c599338c178e7c8999059
choihu <45312821+choihu@users.noreply.github.com>
2019-09-22 04:45:14 -0700

    Fix memcpy function

--- main.cpp
+++ main.cpp
@@ -35,32 +35,19 @@ int main(int argc, char* argv[]) {
 
 
     //print mac address.
-    unsigned char src_mac[6], dest_mac[6];
-
-    memcpy(src_mac, packet+6, 6);
-    memcpy(dest_mac, packet, 6);
-    printf("Source mac is %x:%x:%x:%x:%x:%x and destination mac is %x:%x:%x:%x:%x:%x\n", src_mac[0], src_mac[1], src_mac[2], src_mac[3], src_mac[4], src_mac[5], dest_mac[0], dest_mac[1], dest_mac[2], dest_mac[3], dest_mac[4], dest_mac[5]);
+    printf("Source mac is %x:%x:%x:%x:%x:%x and destination mac is %x:%x:%x:%x:%x:%x\n", packet[6], packet[7], packet[8], packet[9], packet[10], packet[11], packet[0], packet[1], packet[2], packet[3], packet[4], packet[5]);
 
 
     //print ip address.
-    unsigned char is_ip[2];
     unsigned char ip_check[2] = {0x08, 0x00};
-
-    memcpy(is_ip, packet+12, 2);
-    if(!memcmp(is_ip, ip_check, 2)) {
-      unsigned char ip1[4], ip2[4];
-      memcpy(ip1, packet+26, 4);
-      memcpy(ip2, packet+30, 4);
-
-      printf("Source ip is %d.%d.%d.%d and destination ip is %d.%d.%d.%d\n", ip1[0], ip1[1], ip1[2], ip1[3], ip2[0], ip2[1], ip2[2], ip2[3]);
+    if(!memcmp(packet+12, ip_check, 2)) {
+      printf("Source ip is %d.%d.%d.%d and destination ip is %d.%d.%d.%d\n", packet[26], packet[27], packet[28], packet[29], packet[30], packet[31], packet[32], packet[33]);
     
 
       //print tcp port.
-      unsigned char is_tcp[1];
       unsigned char tcp_check[1] = {0x06};
 
-      memcpy(is_tcp, packet+23, 1);
-      if(!memcmp(is_tcp, tcp_check, 1)) {
+      if(!memcmp(packet+23, tcp_check, 1)) {
         unsigned char port1[2], port2[2];
         memcpy(port1, packet+34, 2);
         uint16_t* src_port = (uint16_t*) port1;
